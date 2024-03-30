1b5bb125665b74d769a3aabd4b1c5433ce9b43f6
mymy31 <149076358+mymy31@users.noreply.github.com>
2024-03-30 17:17:25 +0100

    Add files via upload

---
+++ factory materiel/index (5).php
@@ -0,0 +1,563 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>FACTORY MATERIEL</title>
+    <link rel="stylesheet" href="style.css">
+<script type="text/javascript" src="https://me.kis.v2.scr.kaspersky-labs.com/FD126C42-EBFA-4E12-B309-BB3FDD723AC1/main.js?attr=6wVZNjcRwaBaLiWTME4q53O2BiXORq7xY5Zs1qxTXodVOCMZLTelJn0pwYGxVbMZBL4BgGsYIPzYPb4FmWtW9P3bhQHbb5N1b7hqoiphUl5nvtj40Psr5nvPe9sIcaTRYyGhPHYMuh3Wr9aT2a1svar9Duvm0f7B9u4msNN3MdfFzI6Ywl0hO7CL34KVzbEiEGfGQGhbSWuBI3jsrgIQVh8_JzZs_WQRhnxBi90ftUqZY8FRgPWucoVi4jCMeY72D2Iguxdfll8Y_18P7jWwtbuHlBiWIqNql2AU0YN2xPlHcT9dG6TpfcNZ0BNksWVeWeGMIDVJSdlARIz0Yy1Lupp15X8mhVacqWSyt7mExmVd2zJ46uyYhzrmB82Ec-1sYER_Sqthd4jweOGkybXTpBCV0P2vcEMqxsPZWkvAdffLPxyUWUN-fXyMF1eRlh_c9Nkl_Re7OyVF4d8bXMWoquSnM9PiJmMjSSfRA26fDzdCJLt1EncXH7v-PNeOWxAuulj-AWXBMNSV4BmhUkZmWNo7VYCxvbyvh7ezoZuj_Kgq4WUEwGw_Fq14cBdTarpH4t5oO7gc3y8s9NgMS2l5AFcEIqmFx5KfuHOsaDWDFHN087X9BYO8mIr8cacHFP8ndXkIOv-MuDIKlPYl_CqgTZx9dpYzI9xRcXsTlJGZKID3RpajUX-3ksp-GtQHMGzJXBkggNItBuSZ5OBgqHE3hLDLCnkAIKVKQc9QI9q-l1eevPrjzG2Tqom692KW7iwASKbq8iKDPgryfI7k7CgJjXFV3bELBNWyqHMUIvcorPudzAx1QPSFwkkcAKZNio8OsCkd7tMq6ekOjfDxH6WT9Vp-wcKEA10vFRA0xR_Pzfq6h4-MfCiz8GC00SbHe3l4KBP4POa3ZZqQP5_CHJhX8BxEMmoOd_e73zFEf9IxhB5D1VgdqFpOzt40mFB-3YK1UvmfzOlRRPbfR7bjcy_Z2szI-iEHWxgiBGgIaNhrVJ05EwN2zo3Wi0QkB_7S80Q8ACJy82DirGYXvBdmWMgqRC5ajderVhm_nKeKq3KXIoYJRld1zhr7rqavMiYf1RJIldmkxfC5j_5I62ACqIg5SBi9cl0bcynSYG-6vMeC0WS-U8JluSbEEHdrLBwOOA8ZvjkHWwRX8MnU2iYpkQDuW8Esr_XtJPfJm9H8BVGXlKjX82Mr5gEDToVx8j37xuD8g1vKwjrkups1-tHQiLDVz50Z5DZArbB9ZhL9NuPecmEMAIo71lH2XEJjvO7D0oAwmz_Sef9gWrFxZS-TTYR23mgTtHHurABHH2nD1VeDzJaZhccy7Jdo1iJ8PnYQAe-6FtgVBZrVQvrW8Rrrx1RkzsUfUukj3h-4thZ9NppFrb4btI3tKsBiTtrt-UKyhud8MVDx1bfq5tqUXgucnTrULUHIveui-gTtrch5DbD5_5PUKhR-utNQe5oJsrM-pO_nIYz3zADXr-k9GAuGDXRlXJCrS3zThc37TsarZuHWQl8bjVKvBBepw7AdQiM68k6p8U0TCfPcmkSdG4urjY-9O0wPEd3HWsFrL79E_nS_mObvge6O_IIeK4v7Z0YJoxJLkqUAON4skLkVJCtiEp2-Q1NCCCi0-XtsiLeZdbe8CDtPMo4rAE640i62di4J0LGa" nonce="885bb3b3cf8c9f5329c33c62373df4bc" charset="UTF-8"></script></head>
+<body>
+    <header>
+        <div class="logo">
+            <p><span>FACTORY</span>MATERIEL</p>
+        </div>
+        <ul class="menu">
+            <li><a href="#home">Acceuil</a></li>
+            <li><a href="#about_us">A Propos</a></li>
+            <li><a href="#reservation">Contact</a></li>
+          
+        </ul>
+        <div class="toggle_menu"></div>
+    </header>
+    <section id="home">
+        <div class="left">
+            <h4>Nos Appareils au meilleur prix</h4>
+            <h1>Validé par nos clients</h1>
+            <p>Nos appareils sont tester et reconditionnés avec une qualité supérieur.</p>
+             <button><a href="login.php">commander maintenant</a></button>
+        </div>
+        <div class="right">
+            <img src="imagedefond.jpg">
+        </div>
+
+      
+
+   <script>
+       var small_menu = document.querySelector('.toggle_menu')
+       var menu = document.querySelector('.menu')
+
+       small_menu.onclick = function(){
+            small_menu.classList.toggle('active');
+            menu.classList.toggle('responsive');
+       }
+   </script>
+   <style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;500;700&display=swap');
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: 'Poppins', sans-serif; 
+}
+section {
+    margin-top: 50px;
+}
+html {
+    scroll-behavior: smooth;
+    font-size: 62.5%;
+}
+header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    height: 50px;
+    padding: 0 10%;
+    position: fixed;
+    top: 0;
+    left: 0;
+    right: 0;
+    background-color: rgba(255 255 255 /0.9);
+    box-shadow: 0 0 10px rgba(0,0,0,0.2);
+    z-index: 10;
+}
+.menu {
+    display: flex;
+}
+.logo {
+    color: #27ae60;
+    font-weight: 700;
+    font-size: 25px;
+}
+.logo span {
+    color: #273e60;
+}
+.menu li {
+   margin: 0 15px;
+   list-style: none;
+}
+.menu li a {
+    font-size: 14px;
+    text-decoration: 0;
+    color: #999;
+    position: relative;
+}
+.menu li a::before {
+    position: absolute;
+    top: -5px;
+    left: 0;
+    content: "";
+    width: 0;
+    height: 2px;
+    border-radius: 6px;
+    background-color: #27ae60;
+    transition: 0.5s;
+}
+.menu li a:hover::before {
+    width: 100%;
+}
+.menu li a::after {
+    position: absolute;
+    bottom: -5px;
+    right: 0;
+    content: "";
+    width: 0;
+    height: 2px;
+    border-radius: 6px;
+    background-color: #27ae60;
+    transition: 0.5s;
+}
+.menu li a:hover::after {
+    width: 100%;
+}
+.menu li a:hover {
+    color: #000;
+}
+#home {
+    margin-top: 50px;
+    display: flex;
+    align-items: center;
+    margin-left: 10%;
+    margin-right: 10%;
+    height: calc(100vh - 50px);
+    justify-content: space-between;
+}
+#home .left {
+    width: 40%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+}
+#home .left h4 {
+    font-size: 20px;
+    color: #27ae60;
+}
+#home .left h1 {
+    font-size: 40px;
+}
+#home .left p {
+    font-size: 12px;
+    color: #999;
+}
+#home .left button {
+     margin-top: 20px;
+     width: fit-content;
+     padding: 5px 10px;
+     background-color: #000;
+     border: 0;
+     transition: 0.5s;
+     position: relative;
+     cursor: pointer;
+}
+#home .left button  a {
+    text-decoration: 0;
+    color: #fff;
+    font-weight: bold;
+    text-transform: uppercase;
+    transition: 0.5s;
+}
+#home .left button::before {
+    position: absolute;
+    left: 5px;
+    top: 5px;
+    content: "";
+    height: 100%;
+    width: 100%;
+    z-index: -1;
+    background-color: #27ae60;
+}
+#home .left button:hover a{
+   letter-spacing: 1px;
+}
+
+#home .right {
+    width: 50%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+}
+#home .right img {
+    width: 100%;
+    animation: animate 2s linear;
+    transition: 0.5s;
+}
+@keyframes animate {
+    0%{opacity: 0;}
+    100%{opacity: 1;}
+}
+
+#menu {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    background-color: rgba(0,0,0,0.05);
+    padding: 50px 10px;
+}
+.mini_title {
+    margin-top: 25px;
+    font-size: 16px;
+    color: #27ae60;
+    text-transform: capitalize;
+}
+.title {
+    font-size: 20px;
+    text-transform: uppercase;
+    color: #273e60;
+    margin-bottom: 25px;
+    text-align: center;
+}
+.dishes {
+    display: flex;
+    flex-wrap: wrap;
+    width: 80%;
+    justify-content: space-around;
+}
+.dishes .dish {
+    height: 280px;
+    width: 30%;
+    background-color: #fff;
+    padding: 25px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    border-radius: 6px;
+    margin-bottom: 35px;
+    transition: 0.5s;
+    box-shadow: 0 0 10px rgba(0,0,0,0.1);
+
+}
+.dishes .dish  img {
+    width: 130px;
+    height: 120px;
+    margin-bottom: 15px;
+}
+.dishes .dish p {
+    text-align: center;
+    font-size: 18px;
+
+}
+.dishes .dish a {
+    margin-top: 10px;
+    font-size: 14px;
+    text-decoration: 0;
+    background-color: #27ae60;
+    padding: 5px 60px;
+    color: #fff;
+    border-radius: 6px;
+}
+
+::-webkit-scrollbar {
+    width: 8px;
+}
+::-webkit-scrollbar-thumb {
+    background-color: #27ae60;
+}
+#about_us {
+    display: flex;
+    align-items: center;
+    height: calc(100vh - 50px);
+    flex-direction: column;
+    padding: 50px 10%;
+    width: 100%;
+}
+.about {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    justify-content: space-between;
+    height: 70vh;
+}
+.about .left {
+     width: 40%;
+     display: flex;
+     align-items: center;
+     justify-content: center;
+     height: 100vh;
+}
+.about .left img {
+    width: 90%;
+}
+.about .right {
+    width: 60%;
+}
+.about .right h3 {
+    font-size:35px;
+    color: #273e60;
+    margin: 10px 0;
+}
+.about .right p {
+    font-size: 14px ;
+    color: #999;
+}
+.about .right button {
+    margin: 20px 0;
+    padding: 5px 60px;
+    background-color: #27ae60;
+    border: 0;
+}
+.about .right button a{
+    color: #fff;
+    text-decoration: 0;
+    font-weight: bolder;
+}
+.comment_section {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: calc(100vh - 50px);
+    flex-direction: column;
+    padding: 0 10%;
+}
+
+.comments {
+    display: flex;
+    justify-content: space-around;
+    width: 100%;
+    flex-wrap: wrap;
+}
+.comments  .comment div {
+   display: flex;
+   align-items: center;
+   margin-bottom: 10px;
+}
+.comments  .comment div img {
+   width: 45px;
+   height: 45px;
+   border: 2px solid #27ae60;
+   border-radius: 50%;
+}
+.comments  .comment {
+    width: 30%;
+    border: 1px solid #ccc;
+    border-radius: 6px;
+    padding: 20px;
+}
+.comments  .comment div h4 {
+    margin-left: 15px;
+    font-size: 14px;
+    color: #273e60;
+}
+.comments  .comment p {
+    font-size: 13px;
+    color: #999;
+}
+#reservation {
+    margin-top: 50px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 100vh;
+    flex-direction: column;
+}
+#reservation form {
+    display: flex;
+    background-color: rgba(0,0,0,0.05);
+    padding: 25px;
+    border-radius: 6px;
+    flex-direction: column;
+    width: 350px;
+}
+#reservation form  label {
+    margin-bottom: 8px;
+    font-size: 14px;
+}
+#reservation form input {
+    margin-bottom: 10px;
+   padding: 5px;
+   border: 1px solid transparent;
+   box-shadow: 0 0 10px rgba(0,0,0,0.05);
+   outline: 0;
+}
+#reservation form input:focus {
+    outline: 1px solid #27ae60;
+}
+#reservation form input[type="submit"]{
+    margin-top: 15px;
+    color: #fff;
+    background-color: #27ae60;
+    border: 1px solid #27ae60;
+}
+footer {
+    background-color: rgba(0,0,0,0.05);
+}
+footer .services_list {
+    padding: 10px 10%;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: wrap;
+}
+.service img {
+    width: 40px;
+    background-color: #27ae60;
+    padding: 10px;
+    border-radius: 50%;
+    margin-bottom: 10px;
+
+}
+.service {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    margin: 25px auto;
+}
+.service  p {
+    color: #333;
+}
+hr {
+    background-color: #ccc;
+    border: 0;
+    height: 2px;
+    width: 100%;
+}
+.footer_text {
+    text-align: center;
+    font-size: 15px;
+    padding: 8px 0;
+}
+.footer_text span {
+    color: #27ae60;
+}
+.toggle_menu {
+    display: none;
+}
+@media (max-width:964px) {
+    header .menu {
+        display: none;
+    }
+    header {
+        padding: 0 20px;
+    }
+    #home {
+        flex-direction: column;
+        padding: 12px;
+        height: fit-content;
+
+    }
+    #home .left , #home .right  {
+        width: 100%;
+        padding: 0;
+    }
+    #home .left {
+        margin: 100px 0;
+    }
+    #home .left h1 {
+        font-size: 20px;
+        margin-bottom: 15px;
+    }
+    #home .left h4 {
+        font-size: 15px;
+    }
+    #home .left p {
+        font-size: 15px;
+    }
+    #menu .dishes .dish {
+        width: 100%;
+        max-width: 260px;
+    }
+    #about_us {
+        padding: 10px ;
+        height: auto;
+    }
+    #about_us .about {
+        flex-direction: column;
+       height: fit-content;
+    }
+    #about_us .left , #about_us .right {
+        width: 100%;
+    }
+    #about_us .left {
+        height: fit-content;
+    }
+    #about_us .left img {
+        width: 50%;
+    }
+    #about_us .right {
+        text-align: center;
+        padding: 20px;
+    }
+    .comment_section {
+        height: auto;
+    }
+    .comment_section .comments  .comment{
+        width: 100%;
+        margin-top: 20px;
+    }
+    .services_list .service {
+        padding: 0 50px;
+    }
+    .toggle_menu {
+        display: flex;
+        height: 40px;
+        width: 40px;
+         display: flex;
+         align-items: center;
+         justify-content: center;
+         position: relative;
+         cursor: pointer;
+    }
+    .toggle_menu::before {
+        position: absolute;
+        content: "";
+        height: 3px;
+        width: 28px;
+        background-color: #27ae60;
+        border-radius: 6px;
+        box-shadow: 0 10px 0 #27ae60;
+        transform: translateY(-10px);
+        transition: 0.5s;
+    }
+    .toggle_menu.active::before {
+        transform: translateY(0) rotate(135deg);
+        box-shadow: 0 0 0 #27ae60
+    }
+    .toggle_menu::after {
+        position: absolute;
+        content: "";
+        height: 3px;
+        width: 28px;
+        background-color: #27ae60;
+        border-radius: 6px;
+        transform: translateY(10px);
+        transition: 0.5s;
+    }
+    .toggle_menu.active::after {
+        transform: translateY(0) rotate(-135deg);
+      
+    }
+    header .menu.responsive {
+        display: flex;
+        position: absolute;
+        top: 50px;
+        left: 0;
+        width: 100%;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: #fff;
+        box-shadow: 0 0 10px rgba(0,0,0,0.1);
+        flex-direction: column;
+    }
+    header .menu.responsive li {
+        margin: 15px 0;
+    }
+
+}
+   </style>
+</body>
+</html>
